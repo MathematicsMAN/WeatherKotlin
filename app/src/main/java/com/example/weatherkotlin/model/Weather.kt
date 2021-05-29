2ce369d3be467e135064b99468b4d6bbc4e86818
MathematicsMAN <damir_a_z@mail.ru>
2021-05-29 04:22:30 +0300

    HomeWork Lesson 3

--- app/src/main/java/com/example/weatherkotlin/model/Weather.kt
+++ app/src/main/java/com/example/weatherkotlin/model/Weather.kt
@@ -1,9 +1,43 @@
 package com.example.weatherkotlin.model
 
-class Weather(
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Weather(
     val city: City = getDefaultCity(),
     val temperature: Int = 22,
     val feelsLike: Int = 20
-)
+): Parcelable
 
 fun getDefaultCity() = City("Москва", 55.7558, 37.6173)
+
+fun getWorldCities(): List<Weather> {
+    return listOf(
+        Weather(City("Лондон", 51.50853, -0.12574), 1, 2),
+        Weather(City("Токио", 35.6895, 139.69171), 3, 4),
+        Weather(City("Париж", 48.85341, 2.3488), 5, 6),
+        Weather(City("Берлин", 52.5200066, 13.404954), 7, 8),
+        Weather(City("Рим", 41.9027835, 12.4963655), 9, 10),
+        Weather(City("Минск", 53.9045398, 27.5615244), 11, 12),
+        Weather(City("Стамбул", 41.0082376, 28.9783589), 13, 14),
+        Weather(City("Вашингтон", 38.9071923, -77.0368707), 15, 16),
+        Weather(City("Киев", 50.4501, 30.5234), 17, 18),
+        Weather(City("Пекин", 39.9042, 116.4073963), 19, 20)
+    )
+}
+
+fun getRussianCities(): MutableList<Weather> {
+    return  mutableListOf(
+        Weather(City("Москва", 55.755826, 37.6173), 1, 2),
+        Weather(City("Санкт-Петербург", 59.9342802, 30.3350986), 3, 3),
+        Weather(City("Новосибирск", 55.0083526, 82.9357327), 5, 6),
+        Weather(City("Екатеринбург", 56.8389261, 60.6057025), 7, 8),
+        Weather(City("Нижний Новгород", 56.2965039, 43.936059), 9, 10),
+        Weather(City("Казань", 55.8304307, 49.0660806), 11, 12),
+        Weather(City("Челябинск", 55.1644419, 61.4368432), 13, 14),
+        Weather(City("Омск", 54.9884804, 73.3242361), 15, 16),
+        Weather(City("Ростов-на-Дону", 47.2357137, 39.701505), 17, 18),
+        Weather(City("Уфа", 54.7387621, 55.9720554), 19, 20)
+    )
+}
--- app/src/main/java/com/example/weatherkotlin/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/weatherkotlin/viewmodel/MainViewModel.kt
@@ -13,21 +13,19 @@ class MainViewModel(
     ViewModel() {
 
     fun getLiveData() = liveDataToObserve
-//    : LiveData<Any> {
-//        getDataFromLocalSource()
-//        return liveDataToObserve
-//    }
 
-//    fun getWeather() = getDataFromLocalSource()
+    fun getWeatherFromLocalSourceRus() = getDataFromLocalSource(isRussian = true)
+    fun getWeatherFromLocalSourceWorld() = getDataFromLocalSource(isRussian = false)
+    fun getWeatherFromRemoteSource() = getDataFromLocalSource(isRussian = true)
 
-    fun getWeatherFromLocalSource() = getDataFromLocalSource()
-    fun getWeatherFromRemoteSource() = getDataFromLocalSource()
-
-    private fun getDataFromLocalSource() {
+    private fun getDataFromLocalSource(isRussian: Boolean) {
         liveDataToObserve.value = AppState.Loading
         Thread {
             sleep(1000)
-            liveDataToObserve.postValue(AppState.Success(repositoryImpl.getWeatherFromLocalStorage()))
+            liveDataToObserve.postValue(AppState.Success(
+                if (isRussian) repositoryImpl.getWeatherFromLocalStorageRus()
+                else  repositoryImpl.getWeatherFromLocalStorageWorld()
+            ))
         }.start()
     }
 }
